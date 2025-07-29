b9eeaae8aebebe3e9692fb51c9b43e62cecad818
Miguel Pérez Regueiro <67692547+MiguelRegueiro@users.noreply.github.com>
2025-07-29 16:48:25 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -3,7 +3,6 @@ if status is-interactive
     fastfetch
 
     # Abbreviations (better than aliases)
-    abbr -a sshserver "ssh regueiro@100.108.179.88"
     abbr -a update "sudo dnf update"
     abbr -a upgrade "sudo dnf upgrade"
 
